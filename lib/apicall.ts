95eb846a3ee16ace90cf1acc81a3d64d2def93fc
zhangjun <ewalker@live.cn>
2024-10-15 16:03:45 +0800

    update

--- lib/apicall.ts
+++ lib/apicall.ts
@@ -1,5 +1,6 @@
 import { openai, createOpenAI, OpenAIProviderSettings } from '@ai-sdk/openai'
 
+// https://github.com/vercel/ai/blob/main/examples/ai-core/src/stream-text/perplexity.ts
 var custom_openai = createOpenAI({
   compatibility: "strict",
   baseURL: process.env["OPENAI_BASE_URL"] ? process.env["OPENAI_BASE_URL"]: "https://api.openai.com/v1"
